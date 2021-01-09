62649397568dcda8bdf4ea8017553245639da6f8
Arão Domingos <araodomingo04@gmail.com>
2021-01-09 13:46:25 +0100

    Criando views para o cadastro e ver a lista de casa

--- resources/views/seller/house/create.blade.php
+++ resources/views/seller/house/create.blade.php
@@ -3,5 +3,46 @@
 @section('title', 'Cadastrando uma casa')
 
 @section('content')
-    Cadastro
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <form action="" method="post">
+                @csrf
+                <div class="row form-group">
+                    <label>Localização </label>
+                    <input type="text" class="form-control" name="">
+                </div>
+
+                <div class="row form-group">
+                    <label>Preço de Venda </label>
+                    <input type="text" class="form-control" name="preco_venda">
+                </div>
+
+
+                <div class="row form-group">
+                    <label>Preço de Alugel </label>
+                    <input type="text" class="form-control" name="preco_alugel">
+                </div>
+
+                <div class="row form-group">
+                    <label>Detalhes da Casa</label>
+                    <textarea name="detalhes" class="form-control"
+                        placeholder="Tamanho da casa,numero de quartos"></textarea>
+                </div>
+
+                <label>Gender *:</label>
+                <p>
+                    M:
+                    <input type="radio" class="flat" name="gender" id="genderM" value="M" checked="" required /> F:
+                    <input type="radio" class="flat" name="gender" id="genderK" value="K" /> K
+                    <input type="radio" class="flat" name="gender" id="genderF" value="F" />
+                </p>
+
+                <div class="row form-group">
+                    <button type="submit" class="form-control btn btn-primary">Cadastrar uma casa</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 @endsection
--- resources/views/seller/master/app.blade.php
+++ resources/views/seller/master/app.blade.php
@@ -16,7 +16,7 @@
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
-  <link href="{{ asset('css/sb-admin-2.min.css')}}" rel="stylesheet">
+<link href="{{ asset('css/sb-admin-2.min.css')}}" rel="stylesheet">
 
 </head>
 
@@ -299,8 +299,6 @@
   <a class="scroll-to-top rounded" href="#page-top">
     <i class="fas fa-angle-up"></i>
   </a>
-
-
   <!-- Bootstrap core JavaScript-->
   <script src="{{ asset('vendor/jquery/jquery.min.js')}}"></script>
   <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
