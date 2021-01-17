45d9d1a1a9480df0b5df91916f105e45b4f6fc49
Arão Domingos <araodomingo04@gmail.com>
2021-01-17 22:10:40 +0100

    Adicionando o tooltip

--- resources/views/seller/master/app.blade.php
+++ resources/views/seller/master/app.blade.php
@@ -306,7 +306,7 @@
     <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
     <!-- Core plugin JavaScript-->
     <script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js')}}"></script>
-
+    <script src="{{ asset('js/init.js')}}"></script>
     <script src="{{ asset('js/sb-admin-2.min.js')}}"></script>
 
 </body>
